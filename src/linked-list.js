3919bd6e3a1a21884bc1796523451fe97a0561e9
Manuel Cheta <chetamanuel@gmail.com>
2022-09-28 18:17:51 +0100

    Linked list - 1

---
+++ src/linked-list.js
@@ -0,0 +1,33 @@
+// @ts-nocheck
+
+class LinkedList {
+  constructor() {
+    this.head= null;
+    this.length = 0;
+  }
+
+  output() {
+    console.log('hey')
+  }
+
+  insertAtHead(data) {
+    const newNode = new Node(data, this.head);
+    this.head = newNode;
+    this.length++;
+  }
+}
+
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next= next;
+  }
+}
+
+
+
+const trace = new LinkedList();
+trace.insertAtHead(10);
+trace.insertAtHead(20);
+
+console.log(trace);
